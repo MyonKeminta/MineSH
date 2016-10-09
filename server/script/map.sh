0af35219fdd21f205ace7d14b96c066598a2fb29
MyonKeminta <myonkeminta@163.com>
2016-10-09 19:45:27 +0800

    Finished the map generator. What a f***ing project it is!

--- server/script/map.sh
+++ server/script/map.sh
@@ -50,16 +50,49 @@ loadMap()
 		return 1
 	fi
 	_mineMapPath="$1/mine.map"
-	_mineMapPath="$1/state.map"
+	_stateMapPath="$1/state.map"
 
-	# TODO: Finish this
+	mineshInfo "Loading map..."
+
+	local size
+	local data
+
+	{
+		read size
+		_mapWidth=$(cut -d ' ' -f 1 <<< "$size")
+		_mapHeight=$(cut -d ' ' -f 2 <<< "$size")
+		if [[ $_mapWidth -le 0 || $_mapHeight -le 0 ]]; then
+			mineshErr "Failed loading map: Map file invalid."
+			return 1
+		fi
+
+		read data
+		_mineMap=($data)
+	} < "${_mineMapPath}"
+
+	read data < "${_stateMapPath}"
+	_stateMap=($data)
+
+	mineshInfo "Success loading map."
 }
 
 # Save state map into file.
 saveStateMap()
 {
-	# TODO: Implement this.
-	:
+	mineshLog "Saving state map..."
+	if [[ -z $_stateMapPath ]]; then
+		mineshErr "Map path is null. Failed saving state map."
+		return 1
+	fi
+
+	{
+		for (( i = 0; i < _mapHeight*_mapWidth; i++ )); do
+			echo "${_stateMap[$i]} "
+		done
+		echo
+	} > $_stateMapPath
+	mineshLog "Saving state map completed."
+	return 0
 }
 
 # hasMine <x> <y>
@@ -67,8 +100,8 @@ saveStateMap()
 # return 1 otherwise.
 hasMine()
 {
-	# TODO: Implement this.
-	:
+	[[ ${_mineMap[$(getIndexByCell "$1" "$2")]} = '9' ]]
+	return $?
 }
 
 # getCellState <x> <y>
@@ -78,8 +111,7 @@ hasMine()
 # echo ! for flagged cells
 getCellState()
 {
-	# TODO: Implement this.
-	:
+	echo ${_stateMap[$(getIndexByCell "$1" "$2")]}
 }
 
 # isUnknown <x> <y>
@@ -95,15 +127,50 @@ isUnknown()
 # echo 9 if there's a mine in this cell.
 getCellValue()
 {
-	# TODO: Implement this.
-	:
+	# if hasMine "$1" "$2"; then
+	# 	echo 9
+	# 	return 0
+	# fi
+
+	# local adjacent=$((0xff))
+
+	# # Remove left side if x == 0
+	# if [[ $1 -eq 0 ]]; then
+	# 	adjacent=$((adjacent&0x3e))
+	# fi
+
+	# # Remove right side if x == width-1
+	# if [[ $1 -eq $_mapWidth-1 ]]; then
+	# 	adjacent=$((adjacent&0xe3))
+	# fi
+
+	# # Remove top side if y == 0
+	# if [[ $2 -eq 0 ]]; then
+	# 	adjacent=$((adjacent&0x8f))
+	# fi
+
+	# #Remove bottom side if y == height-1
+	# if [[ $2 -eq $_mapHeight-1 ]]; then
+	# 	adjacent=$((adjacent&0xf8))
+	# fi
+
+	# local result=0
+
+	echo ${_mineMap[$(getIndexByCell "$1" "$2")]}
 }
 
 
 # getRegionState <x> <y> <w> <h>
 getRegionState()
 {
-	:
+	local i
+	local j
+	for (( i = 0; i < $4; i++ )); do
+		for (( j = 0; j < $3; j++ )); do
+			echo -n "${_stateMap[$(getIndexByCell $(($1+$j)) $(($2+$i)))]}"
+		done
+	done
+	echo
 }
 
 # triggerCells <x> <y> [...]
@@ -112,7 +179,7 @@ getRegionState()
 triggerCells()
 {
 	while [[ $# -ge 2 ]]; do
-		if [[ $(getCellState "$1" "$2") = '.' ]]; then
+		if isUnknown "$1" "$2"; then
 			_stateMap[$(getIndexByCell "$1" "$2")]=$(getCellValue "$1" "$2")
 		fi
 		shift 2
@@ -146,3 +213,256 @@ putFlags()
 	return $changed
 }
 
+
+
+# getRand <min> <max>
+# Generate a integer in [min, max)
+getRand()
+{
+	local ran=$(dd bs=4 count=1 'if=/dev/urandom' status=none | od --format=u --address-radix=none)
+	echo $((ran%($2-$1)+$1))
+}
+
+
+# generateMap <width> <height> [--mine-rate <rate-percent>] [--block <block-width> <block-height>]
+# A block is a rectangle area on the map.
+# A map can be divided into many blocks and a block can be divided into cells.
+# A cell is the smallest unit of the map.
+# Mines are generated for each block.
+generateMap()
+{
+	local -i rate=25
+	local -i blockWidth=50
+	local -i blockHeight=50
+	local -i width
+	local -i height
+	local -i rows
+	local -i columns
+
+	if [[ $# -lt 2 ]]; then
+		mineshErr "Too few args passed to generateMap"
+		return 1
+	fi
+
+	if ! grep -q -E "^[0-9]+$" <<< $1 || ! grep -q -E "^[0-9]+$" <<< $2 || [[ $1 -lt 1 || $2 -lt 1 ]]; then
+		mineshErr "Invalid map size."
+		return 1
+	fi
+
+	width=$1
+	height=$2
+	shift 2
+
+	while [[ $# -gt 0 ]]; do
+		local current=$1
+		shift
+		case $current in
+			--mine-rate )
+				if [[ $# -lt 1 ]]; then
+					mineshErr "generateMap: Invalid args"
+					return 1
+				fi
+
+				if ! grep -q -E "^[0-9]+$" <<< $1; then
+					mineshErr "generateMap: Invalid args"
+					return 1
+				fi
+
+				rate=$1
+				if [[ $rate -gt 100 ]]; then
+					rate=100
+				fi
+				shift
+				;;
+
+			--block )
+				if [[ $# -lt 2 ]]; then
+					mineshErr "generateMap: Invalid args"
+					return 1
+				fi
+
+				if ! grep -q -E "^[0-9]+$" <<< $1 || ! grep -q -E "^[0-9]+$" <<< $2; then
+					mineshErr "generateMap: Invalid args"
+					return 1
+				fi
+
+				blockWidth=$1
+				blockHeight=$2
+				shift 2
+				;;
+
+			* )
+				mineshErr "generateMap: Invalid args"
+				return 1
+				;;
+		esac
+	done
+
+	if [[ -e "${MINESH_SVR_DATA_DIR}/map" ]]; then
+		mineshInfo "Map file already existed. Are you sure to override them?"
+		if ! confirm; then
+			mineshInfo "Canceled generating map."
+			return 3
+		fi
+
+		safeRmDir "${MINESH_SVR_DATA_DIR}/map"
+		local result=$?
+
+		if [[ $result -ne 0 ]]; then
+			return $result
+		fi
+	fi
+
+	columns=$((width/blockWidth))
+	rows=$((height/blockHeight))
+
+	#Round to integer counts of rows and columns.
+	if [[ ${width}%${blockWidth} -gt 0 ]]; then
+		((++columns))
+		((width=colomns*blockWidth))
+		mineshInfo "Rounded width to ${width}."
+	fi
+	if [[ ${height}%${blockHeight} -gt 0 ]]; then
+		((++rows))
+		((height=rows*blockHeight))
+		mineshInfo "Rounded height to ${height}."
+	fi
+
+	echo "Preparing to generate..."
+
+	local map
+
+	for (( i = 0; i < width*height; i++ )); do
+		map[$i]=0
+	done
+
+	local blockTemp
+
+	for (( i = 0; i < blockWidth*blockHeight; i++ )); do
+		blockTemp[$i]=$i
+	done
+
+	local blockMineCount=$((blockWidth*blockHeight*rate/100))
+
+	local ran
+	local temp
+	local leftBorderFlag
+	local rightBorderFlag
+	local topBorderFlag
+	local bottomBorderFlag
+
+	echo $blockWidth $blockHeight $rows $columns $rate
+
+	echo "Generating mines..."
+
+	for (( i = 0; i < rows; i++ )); do
+		for (( j = 0; j < columns; j++ )); do
+			# Display the progress.
+			echo -en "\rProgress: $(((i*columns+j)*100/(rows*columns)))%     "
+
+			# Randomize the first $blockMineCount elements. No neet to reset.
+			for (( k = 0; k < blockMineCount; k++ )); do
+				ran=$(getRand 0 $((blockWidth*blockHeight)))
+				temp=${blockTemp[$k]}
+				blockTemp[$k]=${blockTemp[$ran]}
+				blockTemp[$ran]=$temp
+			done
+
+			# Put mines into the map.
+			for (( k = 0; k < blockMineCount; k++ )); do
+				# WTF!??
+				temp=$((((i*blockWidth+${blockTemp[$k]}/blockWidth)*columns+j)*blockWidth+${blockTemp[$k]}%blockWidth))
+				[[ $((temp/width)) -eq 0 ]]
+				topBorderFlag=$?
+				[[ $((temp/width)) -eq $((height-1)) ]]
+				bottomBorderFlag=$?
+				[[ $((temp%width)) -eq 0 ]]
+				leftBorderFlag=$?
+				[[ $(((temp+1)%width)) -eq 0 ]]
+				rightBorderFlag=$?
+
+				((map[$temp]+=9))
+				if [[ $leftBorderFlag -ne 0 ]]; then
+					((++map[$temp-1]))
+					if [[ $topBorderFlag -ne 0 ]]; then
+						((++map[$temp-$width-1]))
+					fi
+					if [[ $bottomBorderFlag -ne 0 ]]; then
+						((++map[$temp+$width-1]))
+					fi
+				fi
+
+				if [[ $topBorderFlag -ne 0 ]]; then
+					((++map[$temp-$width]))
+				fi
+
+				if [[ $bottomBorderFlag -ne 0 ]]; then
+					((++map[$temp+$width]))
+				fi
+
+				if [[ $rightBorderFlag -ne 0 ]]; then
+					((++map[$temp+1]))
+					if [[ $topBorderFlag -ne 0 ]]; then
+						((++map[$temp-$width+1]))
+					fi
+					if [[ $bottomBorderFlag -ne 0 ]]; then
+						((++map[$temp+$width+1]))
+					fi
+				fi
+
+			done
+		done
+	done
+
+	echo -e "\rProgress: 100%     "
+
+
+	# # Generate numbers on each cell
+	# echo "Converting data..."
+	# for (( i = 0; i < height; i++ )); do
+	# 	echo -en "\rProgress: $((i*100/height))%     "
+	# 	for (( j = 0; j < width; j++ )); do
+	# 		local index=$((i*width+j))
+
+	# 	done
+	# done
+
+	# echo -e "\rProgress: 100%"
+
+
+	# Save the generated map.
+	echo "Saving..."
+
+	if [[ ! -e "${MINESH_SVR_DATA_DIR}/map" ]]; then
+		mkdir "${MINESH_SVR_DATA_DIR}/map"
+	fi
+
+	{
+		echo "$width $height"
+		for (( i = 0; i < width*height; i++ )); do
+			if [[ ${map[$i]} -gt 9 ]]; then
+				echo -n "9 "
+			else
+				echo -n "${map[$i]} "
+			fi
+		done
+	} > "${MINESH_SVR_DATA_DIR}/map/mine.map"
+	if [[ $? -ne 0 ]]; then
+		mineshErr "Failed Saving the mine map. Generation failed."
+		return 5
+	fi
+
+	{
+		for (( i = 0; i < width*height; i++ )); do
+			echo -n ". "
+		done
+	} > "${MINESH_SVR_DATA_DIR}/map/state.map"
+	if [[ $? -ne 0 ]]; then
+		mineshErr "Failed Saving the state map. Generation failed."
+		return 6
+	fi
+
+	echo "Done."
+
+	return 0
+}
